dfe8266a2218bbdacd1184a874a1675c0bad95c0
Sydney <sydney@artificial.studio>
2020-06-12 14:04:44 +0200

    frontend boilerplate setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Record management frontend</h1>
+    </div>
+  );
+}
+
+export default App;
